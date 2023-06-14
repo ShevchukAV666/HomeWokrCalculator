b9aa09e44d060416d3789b863a11c92b5711426d
ShevchukAV666 <666mog@mail.ru>
2023-06-14 20:34:43 +0500

    Коммит

---
+++ src/main/java/online/calculator/home/work/calculator/HomeWorkCalculatorApplication.java
@@ -0,0 +1,13 @@
+package online.calculator.home.work.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/calculator/home/work/calculator/HomeWorkCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package online.calculator.home.work.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
